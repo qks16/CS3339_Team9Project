d80ef67ed29d745736e518ea4e2fea5cc9a0880d
mrtechfinder77 <spencerjohnson2003@gmail.com>
2026-04-21 11:10:41 -0500

    Add newIM_tester.cpp for instruction memory testing

---
+++ newIM_tester.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "processor.cpp"
+
+using namespace std;
+using namespace processor;
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cerr << "Usage: ./im_test <instruction_memory_size> <assembly_file>\n";
+        return 1;
+    }
+
+    int instruction_memory_size = atoi(argv[1]);
+    Instruction_Memory im(instruction_memory_size);
+    ifstream infile(argv[2]);
+
+    if (!infile.is_open()) {
+        cerr << "Could not open file.\n";
+        return 1;
+    }
+
+    im.loadInstructions(infile);
+    im.printBinaryListing();
+
+    while (im.getPC() < im.size()) {
+        cout << "Executing instruction at PC: " << im.getPC() << " -> " << im.getInstruction() << endl;
+        im.setPC(im.getPC() + 1);
+    }
+
+    return 0;
+}
